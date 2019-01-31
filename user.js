f7223cd3775c8fe4f80f03b5bb59a68ebf380282
Tomoyuki Harada <harada.tomoyuki@jp.fujitsu.com>
2019-01-31 11:02:28 +0900

    initial commit

---
+++ user.js
@@ -0,0 +1,13 @@
+user_pref("layers.acceleration.disabled", false);            // default: false
+user_pref("layers.acceleration.force-enabled", true);        // default: false
+user_pref("gfx.direct2d.disabled", false);                   // default: false
+user_pref("gfx.direct2d.force-enabled", true);               // default: false
+
+user_pref("gfx.font_rendering.cleartype.always_use_for_content", true);    // default: false
+user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);     // default: -1
+user_pref("gfx.font_rendering.cleartype_params.pixel_structure", 1);       // default: -1
+user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);        // default: -1
+user_pref("gfx.font_rendering.directwrite.enabled", true);                 // default: true
+user_pref("gfx.use_text_smoothing_setting", true);                         // default: false
+
+user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 200);   // default: -1

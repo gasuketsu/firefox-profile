9db04c931f75d1a1d2b73348e960875c939df6e1
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2022-02-20 09:27:35 +0900

    chore: tweak cleartype gamma

--- user.js
+++ user.js
@@ -7,7 +7,7 @@ user_pref("gfx.font_rendering.cleartype.always_use_for_content", true);    // de
 user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);     // default: -1
 user_pref("gfx.font_rendering.cleartype_params.pixel_structure", 1);       // default: -1
 user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);        // default: -1
+user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 200);   // default: -1
+user_pref("gfx.font_rendering.cleartype_params.gamma", 500);               // default: -1
 user_pref("gfx.font_rendering.directwrite.enabled", true);                 // default: true
 user_pref("gfx.use_text_smoothing_setting", true);                         // default: false
-
-user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 200);   // default: -1
